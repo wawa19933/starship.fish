fa757ba9ba338e95bbd296824311a6af1a815c45
Sergii Maksymov <maksymov@dexor.de>
2020-05-07 08:38:02 +0200

    Disable auto-install

--- conf.d/starship-init.fish
+++ conf.d/starship-init.fish
@@ -1,10 +1,15 @@
 # Init starship prompt
 #
 # doc: https://starship.rs
-#
+# Quick install: `curl -fsSL https://starship.rs/install.sh | bash`
  
-if not type -q starship
-    curl -fsSL https://starship.rs/install.sh | bash
-end
+# if not type -q starship
+#     curl -fsSL https://starship.rs/install.sh -o /tmp/starship-install.sh
+#     mkdir -p ~/.local/bin
+#     bash /tmp/starship-install.sh -b $HOME/.local/bin -y
+#     rm /tmp/starship-install.sh
+# end
 
-starship init fish | source
+if type -q starship
+   starship init fish | source
+end
